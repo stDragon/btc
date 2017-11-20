5cf66ce96b7fa0164a4a7d369d8eff1e712cd8f5
stDragon <st_Dragon@mail.ru>
2017-11-21 01:45:29 +0400

    обновление пршедших данных

--- js/main.js
+++ js/main.js
@@ -1,31 +1,50 @@
-var App = {}; 
+var db = [];
 
-$(document).ready(function ($) { 
-    App.data = getWexTrades();
-    $('.js-update').on('click', function(e){
+$(document).ready(function ($) {
+    getWexTrades();
+    $('.js-update').on('click', function (e) {
         getWexTrades();
     })
 });
 
 function getWexTrades() {
-    $.ajax({ 
-        type: "GET", 
-        url: "https://wex.nz/api/3/trades/btc_usd", 
+    $.ajax({
+        type: "GET",
+        url: "https://wex.nz/api/3/trades/btc_usd",
         dataType: 'jsonp',
-        async:false,
-        success: function (data){ 
-            var btc = data.btc_usd; 
-            btc.forEach(updateDate);
-            App.data = btc;
-            console.log(App.data);
-        } 
+        async: false,
+        success: updateData
     });
 }
 
-function updateDate(element, index, array) { 
-    return element.humanDate = setHumanDate(element.timestamp); 
-} 
+function updateData(data) {
+    var btcs = data.btc_usd;
+    btcs = _.sortBy(btcs, 'tid');
 
-function setHumanDate(timestamp) { 
-    return new Date(timestamp * 1000).toString(); 
+    if (db.length) {
+        var max = _.max(db, function (data) {
+            return data.tid;
+        });
+        btcs = _.filter(btcs, function (btc) {
+            return btc.tid > max.tid;
+        });
+    }
+
+    btcs.forEach(updateHumanDate);
+    btcs = _.indexBy(btcs, 'tid');
+    btcs = _.toArray(btcs);
+
+    if (btcs.length) {
+        db = db.concat(btcs);
+    }
+
+    console.log(db);
+}
+
+function updateHumanDate(element, index, array) {
+    return element.humanDate = setHumanDate(element.timestamp);
+}
+
+function setHumanDate(timestamp) {
+    return new Date(timestamp * 1000).toString();
 }
